9a9e263790859d761e1d1db822eacf3fe5355347
dudex-computer <duda.mefalmeida@gmail.com>
2025-04-04 23:04:43 -0300

    Maria Eduarda F. de Almeida e Lorena da S. Cani

---
+++ exercicio8/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>TABUADA</title>
+    </head>
+    <body>
+        <fieldset>
+            <legend>TABUADA</legend>
+            <!-- <br> -->
+            <form method="post" action="calcular.php">
+                Digite o número desejado:
+                <input type="text" name="numero" size="25"> 
+                <br> <br>
+                <input type="submit" name="nome" value= "Mostrar a tabuada">
+                <br>
+            </form>
+        </fieldset>
+    </body>
+</html>
